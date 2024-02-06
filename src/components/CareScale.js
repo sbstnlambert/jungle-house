0ca359e90079c9090a06c0c8e8580faba688ed5e
sbstnlambert <sbstn.lambert@gmail.com>
2024-02-06 14:41:09 +0100

    removed comments

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,8 +1,6 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
-// Ici, il s'agit d'une manière de faire.
-//Vous auriez aussi pu utiliser une fonction qui retourne l'élément souhaité, ou bien faire directement des conditions
 const quantityLabel = {
 	1: 'peu',
 	2: 'modérément',
